5cd4377b59eeec54fcb640f20bc4484419064d7a
cainemerrick98 <cainemerrick@gmail.com>
2025-02-11 08:53:13 +0000

    query api does top n

--- celodocs/query_api.py
+++ celodocs/query_api.py
@@ -4,12 +4,24 @@ import os
 import json
 
 if __name__ == '__main__':
+    
     model = SentenceTransformer('all-MiniLM-L6-v2')
     embeddings = np.load(os.path.join(os.getcwd(), 'embeddings.npy'))
-    documents = json.load(os.path.join(os.getcwd(), 'documents.json'))
+    with open(os.path.join(os.getcwd(), 'documents.json'), 'r') as file:
+        documents = json.load(file)
+
     print('Enter a query and see the top documents')
     while True:
         query = input('')
         query = model.encode([query])
-        cosine_similarities = np.dot(embeddings, query.T) / (np.linalg.norm(embeddings, axis=1)) * np.linalg.norm(query)
-        best_matches = np.argpartition(cosine_similarities, -10)[-10:]
+        query = query / np.linalg.norm(query)  # Normalize query vector
+        
+        cosine_similarities = np.dot(embeddings, query.T).squeeze()
+        top_index = np.argsort(cosine_similarities)[-10:][::-1]
+
+        print('\n')
+        for i in top_index:
+            print(documents[i])
+            print('\n')
+        
+        
